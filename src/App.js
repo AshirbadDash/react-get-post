1252636456bcbc63d94f286195f6ba2bd23cbc7a
ashirbad dash <ashirbadash2000@gmail.com>
2024-03-06 16:45:40 +0530

    apis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const getDataFromServer = async () => {
+    const response = await axios.get("http://localhost:8080/call");
+    console.log(response.data);
+    document.getElementById("responsedata").innerHTML = JSON.stringify(
+      response.data
+    );
+  };
+
+  const data = {
+    name: "John",
+    age: 30,
+  };
+
+  // post data to server
+  const postDataFromFrontend = async () => {
+    const response = await axios.post("http://localhost:8080/post", { data });
+    console.log(response.data);
+    document.getElementById("postdata").innerHTML = JSON.stringify(
+      response.data
+    );
+  };
+  //post form data to server
+  const [formData, setFormData] = useState("");
+
+  const postFormDataFromFrontend = async () => {
+    const response = await axios.post("http://localhost:8080/postForm", {
+      formData,
+    });
+    console.log(response.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getDataFromServer}>Get Data</button>
+      <p id="responsedata"></p>
+
+      <button onClick={postDataFromFrontend}>Post Data</button>
+      <p id="postdata"></p>
+
+      <form onSubmit={postFormDataFromFrontend}>
+        <input
+          type="text"
+          name="formData"
+          value={formData}
+          onChange={(e) => setFormData(e.target.value)}
+        />
+        <input type="submit" value="Test Form" />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
